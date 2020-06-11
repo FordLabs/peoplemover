8bbfd909c2744c74a5ad70e3f76aa038c7231829
Michael Rygiel <michael.rygiel1@gmail.com>
2020-06-11 16:00:16 -0400

    [#1] removing unique constraint on product that includes boardid
    Co-authored-by: Michael Rygiel <michael.rygiel1@gmail.com>
    Co-authored-by: Tracey Lo <tslo@umich.edu>
    Co-authored-by: Lee Lazarecky <leelazarecky@gmail.com>
    Co-authored-by: Alex Wojtala <alexwojtala@gmail.com>
    Co-authored-by: Kari Schmitt <schmitt.160@osu.edu>

--- api/src/main/resources/db/migrations/V016__remove_boards.sql
+++ api/src/main/resources/db/migrations/V016__remove_boards.sql
@@ -2,6 +2,9 @@ ALTER TABLE product
 DROP CONSTRAINT FKb2jkw4ykiljlycy09v80e9r3b;
 
 ALTER TABLE product
+DROP CONSTRAINT UKos2grifuk9tqftgcsti9f7f9w;
+
+ALTER TABLE product
 DROP COLUMN board_id;
 
 DROP TABLE board;
