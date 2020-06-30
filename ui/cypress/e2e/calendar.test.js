c655774bcbf12c08c7b5b0ad1b1efea78d1e5216
Ashley Clifton <aclift19@ford.com>
2020-06-30 14:23:20 -0400

    Write cypress test for checking default calendar month and day

---
+++ ui/cypress/e2e/calendar.test.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+
+describe('Calendar', () => {
+    it('Calendar should show current month and day', () => {
+        cy.get('[data-cy=calendarToggle]').as('calendarToggle');
+        const expectedCurrentDate = Cypress.moment().format('MMMM DD, YYYY');
+        cy.get('@calendarToggle').should('contain', expectedCurrentDate);
+
+        cy.log('Open calendar');
+        cy.get('@calendarToggle').click();
+
+        const expectedCurrentMonth = Cypress.moment().format('MMMM');
+        cy.get('.monthText').should('have.text', expectedCurrentMonth);
+
+        const expectedCurrentDay = Cypress.moment().format('DD');
+        cy.get('.react-datepicker__day--today').should('have.text', expectedCurrentDay);
+
+        cy.log('Close calendar');
+        cy.get('@calendarToggle').click();
+    });
+});
