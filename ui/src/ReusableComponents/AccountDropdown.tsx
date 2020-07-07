2490e1bb1538fab6a1937b440c834c9571f421d2
Alex Wojtala <alexwojtala@gmail.com>
2020-07-07 14:01:27 -0400

    #149 Add signout button to user dashboard, refactor header and account dropdown. (#153)
    Co-authored-by: Alex Wojtala <alexwojtala@gmail.com>
    Co-authored-by: Michael Rygiel <michael.rygiel1@gmail.com>

--- ui/src/ReusableComponents/AccountDropdown.tsx
+++ ui/src/ReusableComponents/AccountDropdown.tsx
@@ -9,9 +9,10 @@ import {Redirect} from 'react-router-dom';
 
 interface AccountDropdownProps {
     setCurrentModal(modalState: CurrentModalState): void;
+    hideSpaceButtons?: boolean;
 }
 
-function AccountDropdown({setCurrentModal}: AccountDropdownProps): JSX.Element {
+function AccountDropdown({setCurrentModal, hideSpaceButtons}: AccountDropdownProps): JSX.Element {
 
     const [dropdownFlag, setDropdownFlag] = useState<boolean>(false);
     const [redirect, setRedirect] = useState<JSX.Element>();
@@ -34,14 +35,17 @@ function AccountDropdown({setCurrentModal}: AccountDropdownProps): JSX.Element {
 
     return (
         <button data-testid="editContributorsModal" className={'editContributorsModal'} onClick={showsDropdown}>
-            <i className="fas fa-user" data-testid={'userIcon'}></i>
+            <i className="fas fa-user" data-testid={'userIcon'}/>
             <i className="fas fa-caret-down drawerCaret"/>
 
             {dropdownFlag && <div className={'dropdown-container'}>
-                <div className="account-dropdown-options" onClick={() => setCurrentModal({modal: AvailableModals.EDIT_CONTRIBUTORS})}>
-                    Invite Contributors
-                </div>
-                <div className="account-dropdown-options" onClick={() => clearAccessTokenCookie()}>
+                {process.env.REACT_APP_INVITE_USERS_TO_SPACE_ENABLED === 'true' && !hideSpaceButtons &&
+                    <div data-testid="invite-contributors" className="account-dropdown-options"
+                        onClick={() => setCurrentModal({modal: AvailableModals.EDIT_CONTRIBUTORS})}>
+                        Invite Contributors
+                    </div>
+                }
+                <div data-testid="sign-out" className="account-dropdown-options" onClick={() => clearAccessTokenCookie()}>
                     Sign Out
                 </div>
             </div>
