110c3a49ee38e0def44e389979746ec17357ec99
Alex Wojtala <alexwojtala@gmail.com>
2020-07-10 15:49:54 -0400

    Revert "[#164] Replace using scopes from Authquest with mapping DB call"
    Reverting work where we pull from database instead of AuthQuest until we
    get invite users to store data in the database as well
    
    This reverts commit 16aaf760b47c07d1a56bacbd9f0ec35780d0f837.

--- api/src/main/kotlin/com/ford/internalprojects/peoplemover/auth/UserSpaceMappingRepository.kt
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/auth/UserSpaceMappingRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface UserSpaceMappingRepository : JpaRepository<UserSpaceMapping, Int> {
-    fun findAllByUserId(userId: String): List<UserSpaceMapping>
 }
