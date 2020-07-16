8997d5619c176f654d466a7b054c70cfcd7ae0a0
tslo11 <tslo@umich.edu>
2020-07-16 10:22:05 -0400

    #25 download report (#184)
    * [#25] added Download Report to Account Dropdown.
    * [#25] updated ReportGenerator to only export assignments for given date.
    
    Co-authored-by: Tracey Lo <tslo@umich.edu>
    Co-authored-by: Alex Wojtala <alexwojtala@gmail.com>

--- ui/src/ReusableComponents/AccountDropdown.tsx
+++ ui/src/ReusableComponents/AccountDropdown.tsx
@@ -6,13 +6,24 @@ import {AvailableModals, setCurrentModalAction} from '../Redux/Actions';
 import {connect} from 'react-redux';
 import Cookies from 'universal-cookie';
 import {Redirect} from 'react-router-dom';
+import ReportClient from "../Reports/ReportClient";
+import {Space} from "../SpaceDashboard/Space";
+import {GlobalStateProps} from "../Redux/Reducers";
 
 interface AccountDropdownProps {
+    currentSpace: Space;
+    viewingDate: Date;
+
     setCurrentModal(modalState: CurrentModalState): void;
     hideSpaceButtons?: boolean;
 }
 
-function AccountDropdown({setCurrentModal, hideSpaceButtons}: AccountDropdownProps): JSX.Element {
+function AccountDropdown({
+    currentSpace,
+    viewingDate,
+    setCurrentModal,
+    hideSpaceButtons,
+}: AccountDropdownProps): JSX.Element {
 
     const [dropdownFlag, setDropdownFlag] = useState<boolean>(false);
     const [redirect, setRedirect] = useState<JSX.Element>();
@@ -45,6 +56,12 @@ function AccountDropdown({setCurrentModal, hideSpaceButtons}: AccountDropdownPro
                         Invite Contributors
                     </div>
                 }
+                {!hideSpaceButtons &&
+                    <div data-testid="download-report" className="account-dropdown-options"
+                        onClick={async () => { await ReportClient.getReportsWithNames(currentSpace.name, viewingDate) } }>
+                        Download Report
+                    </div>
+                }
                 <div data-testid="sign-out" className="account-dropdown-options" onClick={() => clearAccessTokenCookie()}>
                     Sign Out
                 </div>
@@ -54,8 +71,13 @@ function AccountDropdown({setCurrentModal, hideSpaceButtons}: AccountDropdownPro
     );
 }
 
+const mapStateToProps = (state: GlobalStateProps) => ({
+    currentSpace: state.currentSpace,
+    viewingDate: state.viewingDate,
+});
+
 const mapDispatchToProps = (dispatch: Dispatch) => ({
     setCurrentModal: (modalState: CurrentModalState) => dispatch(setCurrentModalAction(modalState)),
 });
 
-export default connect(null, mapDispatchToProps)(AccountDropdown);
+export default connect(mapStateToProps, mapDispatchToProps)(AccountDropdown);
