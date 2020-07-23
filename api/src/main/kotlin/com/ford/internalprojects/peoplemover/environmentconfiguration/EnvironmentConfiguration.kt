d596da815461b52c48131892a3a211559a2d2870
tslo11 <tslo@umich.edu>
2020-07-23 15:52:54 -0400

    109 backend feature toggle (#218)
    * [#109] added front-end configs to back-end EnvironmentConfiguration endpoint
    
    Co-authored-by: Alex Wojtala <alexwojtala@gmail.com>
    Co-authored-by: Tracey Lo <tslo@umich.edu>

---
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/environmentconfiguration/EnvironmentConfiguration.kt
@@ -0,0 +1,17 @@
+package com.ford.internalprojects.peoplemover.environmentconfiguration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "react.app")
+class EnvironmentConfiguration {
+    var authquest_url: String = ""
+    var authquest_client_id: String = ""
+    var auth_enabled: Boolean = false
+    var invite_users_to_space: Boolean = false
+    var adfs_enabled: Boolean = false
+    var adfs_url_template: String = ""
+    var adfs_client_id: String = ""
+    var adfs_resource: String = ""
+}
