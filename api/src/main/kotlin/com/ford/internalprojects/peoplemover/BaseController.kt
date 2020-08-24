0bc3078c39a91d00e0cdf603d591240e6cb1f02c
Nick Reuter <thenewimagineer@gmail.com>
2020-08-24 16:34:30 -0400

    Ensure BaseController only renders via e2e-test profile
    Co-authored-by: Nick Reuter <thenewimagineer@gmail.com>
    Co-authored-by: Ashley Clifton <randomashes@gmail.com>

--- api/src/main/kotlin/com/ford/internalprojects/peoplemover/BaseController.kt
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/BaseController.kt
@@ -1,11 +1,13 @@
 package com.ford.internalprojects.peoplemover
 
+import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@Profile("e2e-test")
 @RequestMapping("/")
 class BaseController {
 
