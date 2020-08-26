1a46cd713eb262d1fd1c8b5a6a535363b9fe8ba7
Nick Reuter <thenewimagineer@gmail.com>
2020-08-26 10:38:56 -0400

    Prevent page refreshing when adding a space
    Co-authored-by: Nick Reuter <thenewimagineer@gmail.com>
    Co-authored-by: Ashley Clifton <randomashes@gmail.com>

--- ui/src/SpaceDashboard/CreateSpaceForm.tsx
+++ ui/src/SpaceDashboard/CreateSpaceForm.tsx
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import React, {useState} from 'react';
+import React, {FormEvent, useState} from 'react';
 import {Dispatch} from 'redux';
 import {closeModalAction} from '../Redux/Actions';
 import {connect} from 'react-redux';
@@ -33,17 +33,17 @@ function CreateSpaceForm({
     onSubmit,
     closeModal,
 }: CreateSpaceFormProps): JSX.Element {
-
     const maxLength = 40;
     const [spaceName, setSpaceName] = useState<string>('');
 
-    async function addSpace(): Promise<void> {
+    async function addSpace(event: FormEvent): Promise<void> {
+        event.preventDefault();
         const cookies = new Cookies();
         const accessToken = cookies.get('accessToken');
 
         await SpaceClient.createSpaceForUser(spaceName, accessToken);
 
-        onSubmit();
+        await onSubmit();
         closeModal();
     }
 
@@ -51,23 +51,25 @@ function CreateSpaceForm({
         setSpaceName(event.target.value);
     }
 
-    return <form className="createSpaceContainer" onSubmit={addSpace}>
-        <label className="createSpaceLabel" htmlFor="spaceNameField">Space Name</label>
-        <input className="createSpaceInputField"
-            id="spaceNameField"
-            type="text"
-            data-testid="createSpaceInputField"
-            maxLength={maxLength}
-            value={spaceName}
-            onChange={onSpaceNameFieldChanged}/>
-        <span className={`createSpaceFieldText ${spaceName.length >= maxLength ? 'createSpaceFieldTooLong' : ''}`} data-testid="createSpaceFieldText">
-            {spaceName.length} ({maxLength} characters max)
-        </span>
-        <div className="createSpaceButtonContainer">
-            <button className="createSpaceCancelButton" type="button" onClick={closeModal}>Cancel</button>
-            <button className="createSpaceSubmitButton" type="submit" disabled={spaceName.length <= 0}>Add Space</button>
-        </div>
-    </form>;
+    return (
+        <form className="createSpaceContainer" onSubmit={addSpace}>
+            <label className="createSpaceLabel" htmlFor="spaceNameField">Space Name</label>
+            <input className="createSpaceInputField"
+                id="spaceNameField"
+                type="text"
+                data-testid="createSpaceInputField"
+                maxLength={maxLength}
+                value={spaceName}
+                onChange={onSpaceNameFieldChanged}/>
+            <span className={`createSpaceFieldText ${spaceName.length >= maxLength ? 'createSpaceFieldTooLong' : ''}`} data-testid="createSpaceFieldText">
+                {spaceName.length} ({maxLength} characters max)
+            </span>
+            <div className="createSpaceButtonContainer">
+                <button className="createSpaceCancelButton" type="button" onClick={closeModal}>Cancel</button>
+                <button className="createSpaceSubmitButton" type="submit" disabled={spaceName.length <= 0}>Add Space</button>
+            </div>
+        </form>
+    );
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
