df4f54cfdd4a493bf6069e1a1d26e8dc3d1fb5b6
Ashley Clifton <randomashes@gmail.com>
2020-08-27 13:25:10 -0400

    Test additional fields in create new product post response

--- ui/cypress/e2e/product.test.js
+++ ui/cypress/e2e/product.test.js
@@ -24,7 +24,16 @@ describe('Product', () => {
 
         cy.wait('@postNewProduct').should(xhr => {
             expect(xhr?.status).to.equal(200);
-            expect(xhr?.response?.body.name).to.equal(product.name);
+            const body = xhr?.response?.body;
+            expect(body.name).to.equal(product.name);
+            expect(body.archived).to.equal(product.archived);
+            expect(body.spaceLocation.name).to.equal(product.location);
+            expect(body.startDate).to.equal(product.startDate.format('yyyy-MM-DD'));
+            expect(body.endDate).to.equal(product.nextPhaseDate.format('yyyy-MM-DD'));
+            expect(body.notes).to.equal(product.notes);
+            body.productTags.forEach(tag => {
+                expect(product.tags).to.contain(tag.name);
+            });
         });
 
         cy.contains(product.name).parentsUntil('[data-testid=productCardContainer]')
@@ -60,7 +69,7 @@ const populateProductForm = ({ name, location, tags = [], startDate, nextPhaseDa
     const today = Cypress.moment();
     cy.get(dateSelector(today)).click({force: true});
 
-    cy.get('@calendarStartDate').should('have.value', startDate);
+    cy.get('@calendarStartDate').should('have.value', startDate.format('MM/DD/yyyy'));
     cy.get('.modalTitle').click();
 
     cy.get('@calendarEndDate')
@@ -70,7 +79,7 @@ const populateProductForm = ({ name, location, tags = [], startDate, nextPhaseDa
     const tomorrow = Cypress.moment().add(1, 'days');
     cy.get(dateSelector(tomorrow)).click({force: true});
 
-    cy.get('@calendarEndDate').should('have.value', nextPhaseDate);
+    cy.get('@calendarEndDate').should('have.value', nextPhaseDate.format('MM/DD/yyyy'));
 
     cy.get('[data-testid=formNotesToField]')
         .focus()
