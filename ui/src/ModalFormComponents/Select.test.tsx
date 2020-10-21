9bee46469219b2c028990e736981061d31d1417a
Michael Rygiel <michael.rygiel1@gmail.com>
2020-10-21 10:21:41 -0400

    Color dropdown keyboard & screen reader accessiblity (#475)
    Co-authored-by: Ashley Clifton <aclift19@ford.com>
    Co-authored-by: Michael Rygiel <michael.rygiel1@gmail.com>

--- ui/src/ModalFormComponents/Select.test.tsx
+++ ui/src/ModalFormComponents/Select.test.tsx
@@ -18,7 +18,11 @@ import * as React from 'react';
 import Select from './Select';
 import {render, RenderResult, fireEvent} from '@testing-library/react';
 
+jest.useFakeTimers();
+
 describe('Select', () => {
+    const setTimeoutTime = 100;
+    const [upKey, downKey, enterKey] = [38, 40, 13];
 
     let component: RenderResult;
     let onChange: jest.Mock;
@@ -26,14 +30,17 @@ describe('Select', () => {
         {
             value: 0,
             displayValue: <span>Zero</span>,
+            ariaLabel: 'ariaLabel0',
         },
         {
             value: 1,
             displayValue: <span>One</span>,
+            ariaLabel: 'ariaLabel1',
         },
         {
             value: 2,
             displayValue: <span>Two</span>,
+            ariaLabel: 'ariaLabel2',
         },
     ];
     
@@ -41,25 +48,103 @@ describe('Select', () => {
         onChange = jest.fn();
         component = render(
             <Select
+                ariaLabel="selectAriaLabel"
                 selectedOption={options[0]}
                 options={options}
                 onChange={onChange}/>
         );
     });
-    
-    it('should display a newly selected option', () => {
-        fireEvent.click(component.getByText('Zero'));
-        fireEvent.click(component.getByText('Two'));
 
-        expect(component.queryByText('Zero')).toBeNull();
-        expect(component.queryByText('One')).toBeNull();
-        expect(component.queryByText('Two')).not.toBeNull();
+    describe('onClick', () => {
+        beforeEach(() => {
+            const selectDropdownButton = component.getByText('Zero');
+            fireEvent.click(selectDropdownButton);
+            jest.advanceTimersByTime(setTimeoutTime);
+            const selectDropdownOption = component.getByText('Two');
+            fireEvent.click(selectDropdownOption);
+            jest.advanceTimersByTime(setTimeoutTime);
+        });
+
+        it('should display a newly selected option on click', () => {
+            expect(component.queryByText('Zero')).toBeNull();
+            expect(component.queryByText('One')).toBeNull();
+            const expectedDropdownButton = component.queryByText('Two');
+            expect(expectedDropdownButton).not.toBeNull();
+        });
+
+        it('should notify watcher of value change on click', () => {
+            expect(onChange).toHaveBeenCalledWith(options[2]);
+        });
     });
 
-    it('should notify watcher of value change', () => {
-        fireEvent.click(component.getByText('Zero'));
-        fireEvent.click(component.getByText('Two'));
 
-        expect(onChange).toHaveBeenCalledWith(options[2]);
+    describe('onKeyDown', () => {
+        beforeEach(() => {
+            const selectDropdownButton = component.getByText('Zero');
+            fireEvent.keyDown(selectDropdownButton, {keyCode: enterKey});
+            jest.advanceTimersByTime(setTimeoutTime);
+
+            const selectedOptionZero = component.getByTestId('selectOption__0');
+            expect(selectedOptionZero.className).toContain('focused');
+            fireEvent.keyDown(component.getByTestId('selectDropdownOptions'), {keyCode: downKey});
+
+            fireEvent.keyDown(component.getByTestId('selectDropdownOptions'), {keyCode: downKey});
+
+            const selectedOptionTwo = component.getByTestId('selectOption__2');
+            expect(selectedOptionTwo.className).toContain('focused');
+
+            fireEvent.keyDown(component.getByTestId('selectDropdownOptions'), {keyCode: upKey});
+
+            const selectedOptionOne = component.getByTestId('selectOption__1');
+            expect(selectedOptionOne.className).toContain('focused');
+
+            fireEvent.keyDown(component.getByTestId('selectDropdownOptions'), {keyCode: enterKey});
+            jest.advanceTimersByTime(setTimeoutTime);
+        });
+
+        it('should display a newly selected option on keydown', () => {
+            expect(component.queryByText('Zero')).toBeNull();
+            const expectedDropdownButton = component.queryByText('One');
+            expect(expectedDropdownButton).not.toBeNull();
+            expect(component.queryByText('Two')).toBeNull();
+        });
+
+        it('should notify watcher of value change on keydown', () => {
+            expect(onChange).toHaveBeenCalledWith(options[1]);
+        });
+    });
+
+    describe('Aria Labels', () => {
+        beforeEach(() => {
+            const selectDropdownButton = component.getByText('Zero');
+            fireEvent.click(selectDropdownButton);
+            jest.advanceTimersByTime(setTimeoutTime);
+        });
+
+        it('should have aria labels on the dropdown toggle button', () => {
+            component.getByLabelText('selectAriaLabel Selector: ariaLabel0 is selected');
+        });
+
+        it('should have aria labels on the dropdown options list', () => {
+            component.getByLabelText('selectAriaLabel Options');
+        });
+
+        it('should have aria labels on the dropdown options', () => {
+            component.getByLabelText('ariaLabel0');
+            component.getByLabelText('ariaLabel1');
+            component.getByLabelText('ariaLabel2');
+        });
+
+        it('should have aria-selected to be true on the active option', () => {
+            const options = component.getAllByRole('option');
+
+            options.forEach((option) => {
+                if (option.innerHTML.includes('Zero')) {
+                    expect(option.getAttribute('aria-selected')).toBe('true');
+                } else {
+                    expect(option.getAttribute('aria-selected')).toBe('false');
+                }
+            });
+        });
     });
 });
