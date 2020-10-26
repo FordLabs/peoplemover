109f7472aa601ad3be2a46b0fef92bb051bfc871
Michael Rygiel <michael.rygiel1@gmail.com>
2020-10-26 13:17:29 -0400

    Fix imports

--- ui/src/Products/ProductCard.tsx
+++ ui/src/Products/ProductCard.tsx
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import React, {RefObject, useEffect, useState} from 'react';
+import React, {RefObject, useEffect} from 'react';
 import './Product.scss';
 import {connect} from 'react-redux';
 import {
