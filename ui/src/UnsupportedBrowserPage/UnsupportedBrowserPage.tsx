3cef1027a91a1e7732004b173a1e543b3da23411
Michael Rygiel <michael.rygiel1@gmail.com>
2020-10-28 15:37:12 -0400

    Show unsupported browser page when using IE, Edge, or Safari (#505)
    Co-authored-by: Michael Rygiel <michael.rygiel1@gmail.com>
    Co-authored-by: Alex Wojtala <alexwojtala@gmail.com>

---
+++ ui/src/UnsupportedBrowserPage/UnsupportedBrowserPage.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+
+import './UnsupportedBrowserPage.scss';
+import Chrome from './chrome.png';
+import FireFox from './firefox.png';
+import PeopleMoverLogo from '../ReusableComponents/PeopleMoverLogo';
+import Branding from '../ReusableComponents/Branding';
+
+function UnsupportedBrowserPage(): JSX.Element {
+
+    return (
+        <div className="unsupported-browser-container">
+            <div className="peoplemover-logo">
+                <PeopleMoverLogo />
+            </div>
+            <div className="unsupported-browser-text">
+                For the best PeopleMover experience, <br/> please use Chrome
+                <img className="unsupported-browser-img" src={Chrome} alt="Chrome Logo"/> or Firefox
+                <img className="unsupported-browser-img" src={FireFox} alt="FireFox Logo"/>
+            </div>
+            <Branding brand="FordLabs" message="Powered by"/>
+        </div>
+    );
+}
+export default UnsupportedBrowserPage;
