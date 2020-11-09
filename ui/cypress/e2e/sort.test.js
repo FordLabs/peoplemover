a4f6445038ddfd9d4fa9af2089e6e46272b62f12
chris dorman <cdorman1@ford.com>
2020-11-09 13:43:10 -0500

    Add tests for sorting by product tag and location
    Co-authored-by: chris dorman <cdorman1@ford.com>
    Co-authored-by: Alex Wojtala <alexwojtala@gmail.com>

--- ui/cypress/e2e/sort.test.js
+++ ui/cypress/e2e/sort.test.js
@@ -25,4 +25,48 @@ describe('Sort', () => {
 
             });
     });
+
+    it('Sort products by location', () => {
+        cy.get('[data-testid=productCardContainer__my_product]');
+        cy.get('[data-testid=productCardContainer__baguette_bakery]');
+
+        cy.selectOptionFromReactSelect('[data-testid=sortBy]', 'Location')
+            .then(() => {
+                cy.get('[data-testid=productListGroupedContainer]')
+                    .find('[data-testid=productGroup]')
+                    .should('have.length', 2)
+                    .eq(0)
+                    .should('contain', 'Baguette Bakery')
+                    .should('contain', 'location1');
+
+                cy.get('[data-testid=productListGroupedContainer]')
+                    .find('[data-testid=productGroup]')
+                    .eq(1)
+                    .should('contain', 'My Product')
+                    .should('contain', 'No Location');
+
+            });
+    });
+
+    it('Sort products by product tag', () => {
+        cy.get('[data-testid=productCardContainer__my_product]');
+        cy.get('[data-testid=productCardContainer__baguette_bakery]');
+
+        cy.selectOptionFromReactSelect('[data-testid=sortBy]', 'Product Tag')
+            .then(() => {
+                cy.get('[data-testid=productListGroupedContainer]')
+                    .find('[data-testid=productGroup]')
+                    .should('have.length', 2)
+                    .eq(0)
+                    .should('contain', 'My Product')
+                    .should('contain', 'productTag1');
+
+                cy.get('[data-testid=productListGroupedContainer]')
+                    .find('[data-testid=productGroup]')
+                    .eq(1)
+                    .should('contain', 'Baguette Bakery')
+                    .should('contain', 'No Product Tag');
+
+            });
+    });
 });
