695b6249efc8872d43cfa327064ef658134625cc
Ashley Clifton <aclift19@ford.com>
2020-11-24 11:50:56 -0500

    Added created an last modified columns to user space mapping
    Co-authored-by: Ashley Clifton <aclift19@ford.com>
    Co-authored-by: chris dorman <cdorman1@ford.com>

--- api/src/main/kotlin/com/ford/internalprojects/peoplemover/auth/UserSpaceMapping.kt
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/auth/UserSpaceMapping.kt
@@ -1,9 +1,16 @@
 package com.ford.internalprojects.peoplemover.auth
 
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
-data class UserSpaceMapping(
+@EntityListeners(AuditingEntityListener::class)
+data class UserSpaceMapping  (
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Int? = null,
@@ -12,5 +19,14 @@ data class UserSpaceMapping(
         val userId: String?,
 
         @Column(name = "space_id")
-        val spaceId: Int?
+        val spaceId: Int?,
+
+        @Column(name = "last_modified_date")
+        @UpdateTimestamp
+        val lastModifiedDate: LocalDateTime? = null,
+
+        @Column(name = "created_date")
+        @CreationTimestamp
+        val createdDate: LocalDateTime? = null
+
 )
