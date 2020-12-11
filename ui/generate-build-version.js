22decf23ef2a1ae7f3cca1683110d5f452a8cafe
Ashley Clifton <aclift19@ford.com>
2020-12-11 09:38:31 -0500

    Remove returning console log generate-build-version
    Co-authored-by: Ashley Clifton <aclift19@ford.com>
    Co-authored-by: Vianney Willot <vianney.willot@gmail.com>
    Co-authored-by: Elise Griffiths <egriff38@ford.com>

--- ui/generate-build-version.js
+++ ui/generate-build-version.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
-/* generate-build-version.js */
-
 const fs = require('fs');
 const packageJson = require('./package.json');
 
@@ -15,7 +13,8 @@ let jsonContent = JSON.stringify(jsonData);
 fs.writeFile('./public/meta.json', jsonContent, 'utf8', function(err) {
     if (err) {
         console.log('An error occurred while writing JSON Object to meta.json');
-        return console.log(err);
+        console.log(err)
+        return err;
     }
 
     console.log('meta.json file has been saved with latest version number');
