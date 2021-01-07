6ed437bb4f8f60307053d7484446d4878f1a844f
Ashley Clifton <aclift19@ford.com>
2021-01-07 16:15:49 -0500

    Refactor authInterceptor for readability
    Co-authored-by: Ashley Clifton <aclift19@ford.com>
    Co-authored-by: Chris Atkinson <catkin24@ford.com>

--- api/src/main/kotlin/com/ford/internalprojects/peoplemover/auth/AuthInterceptor.kt
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/auth/AuthInterceptor.kt
@@ -16,44 +16,39 @@ class CustomPermissionEvaluator(
         private val userSpaceMappingRepository: UserSpaceMappingRepository,
         private val spaceRepository: SpaceRepository
 ) : PermissionEvaluator {
-
-    @Value("\${read-only-off-flag:false}")
-    private val readOnlyOff: Boolean = false
-
-
-    override fun hasPermission(
-            auth: Authentication, targetDomainObject: Any, permission: Any): Boolean {
+    override fun hasPermission(auth: Authentication, targetDomainObject: Any, permission: Any): Boolean {
         val mapping = userSpaceMappingRepository.findByUserIdAndSpaceId(auth.name, Integer.parseInt(targetDomainObject.toString()))
         return mapping.isPresent
     }
 
-    override fun hasPermission(
-            auth: Authentication, targetId: Serializable, targetType: String, permission: Any): Boolean {
+    override fun hasPermission(auth: Authentication, targetId: Serializable, targetType: String, permission: Any): Boolean {
         val targetIdString = targetId.toString()
 
-        val currentSpace: Space? = if (targetType == "uuid") {
-            spaceRepository.findByUuid(targetIdString)
-        } else {
-            spaceRepository.findByIdOrNull(targetIdString.toInt())
-        }
+        val currentSpace: Space? = getCurrentSpaceByIdOrUuid(targetType, targetIdString)
 
-        if (permission == "write" || permission == "modify") {
-            if (currentSpace == null) {
-                return false
-            }
-            return userSpaceMappingRepository.findByUserIdAndSpaceId(auth.name, currentSpace.id!!).isPresent
+        return if (permission == "write" || permission == "modify") {
+            handleWritePermissions(currentSpace, auth)
         } else if (permission == "read") {
-            if (currentSpace == null) {
-                throw SpaceNotExistsException()
-            }
-            return if (currentSpace.currentDateViewIsPublic) {
-                true
-            } else {
-                userSpaceMappingRepository.findByUserIdAndSpaceId(auth.name, currentSpace.id!!).isPresent
-            }
+            handleReadPermissions(currentSpace, auth)
         } else {
-            return false
+            false
         }
+    }
+
+    private fun handleReadPermissions(currentSpace: Space?, auth: Authentication): Boolean {
+        if (currentSpace == null) throw SpaceNotExistsException()
+        return if (currentSpace.currentDateViewIsPublic) true
+        else userSpaceMappingRepository.findByUserIdAndSpaceId(auth.name, currentSpace.id!!).isPresent
+    }
+
+    private fun handleWritePermissions(currentSpace: Space?, auth: Authentication): Boolean {
+        return if (currentSpace == null) false
+        else userSpaceMappingRepository.findByUserIdAndSpaceId(auth.name, currentSpace.id!!).isPresent
+    }
+
+    private fun getCurrentSpaceByIdOrUuid(targetType: String, targetIdString: String): Space? {
+        return if (targetType == "uuid") spaceRepository.findByUuid(targetIdString)
+        else spaceRepository.findByIdOrNull(targetIdString.toInt())
     }
 }
 
