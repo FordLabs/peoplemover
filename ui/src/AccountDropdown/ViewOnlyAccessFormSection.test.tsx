1c540d95a779e9dcb12358eb25d6895a0d2e249a
Chris Atkinson <christopher.w.atkinson.1989@gmail.com>
2021-01-28 15:49:17 -0500

    Update newly-added files' copyright disclaimers for 2021
    Co-authored-by: Chris Atkinson <christopher.w.atkinson.1989@gmail.com>
    Co-authored-by: Ashley Clifton <aclift19@ford.com>

--- ui/src/AccountDropdown/ViewOnlyAccessFormSection.test.tsx
+++ ui/src/AccountDropdown/ViewOnlyAccessFormSection.test.tsx
@@ -1,6 +1,5 @@
 /*
- *
- *  Copyright (c) 2020 Ford Motor Company
+ *  Copyright (c) 2021 Ford Motor Company
  *  All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,6 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
  */
 
 import TestUtils, {renderWithRedux} from '../tests/TestUtils';
