e13c2baeb6f2f91e9e6dbed7bb272292546626ca
Vianney Willot <vianney.willot@gmail.com>
2021-02-23 11:04:45 -0500

    Put everything in the call back, linter won

--- ui/src/ReusableComponents/AccessibleDropdownContainer.tsx
+++ ui/src/ReusableComponents/AccessibleDropdownContainer.tsx
@@ -36,51 +36,51 @@ export default function AccessibleDropdownContainer({handleClose, ariaLabelledBy
 
     const dropdownContainer = createRef<HTMLDivElement>();
 
-    const setFocusOnExpectedElementWhenUsingUpOrDownKey = (e: { target: EventTarget | null; key?: string }): void => {
-        if (isArrowKeyFunctionalitySetup()) {
-            const movementDirection = getMovementDirection(e.key);
-            setFocusState(e, movementDirection);
-        }
-    };
-
-    const isArrowKeyFunctionalitySetup = (): boolean => {
-        const childrenWithUndefinedId = React.Children.toArray(children).filter(child => (child as ReactElement).props.id === undefined);
-        // @ts-ignore
-        const childrenWithNullRef = React.Children.toArray(children).filter(child => (child as ReactElement).ref === null);
-
-        return childrenWithUndefinedId.length === 0 && childrenWithNullRef.length === 0;
-    };
-
-    const getMovementDirection = (key?: string ): number => {
-        if (key === 'ArrowDown') {
-            return 1;
-        } else {
-            return -1;
-        }
-    };
-
-    const setFocusState = (e: { target: EventTarget | null; key?: string }, movementDirection: number): void => {
-        let target = e.target as HTMLElement;
-
-        const totalNumberOfChildren = React.Children.count(children);
-        const childIndex = React.Children.toArray(children).findIndex(child => (child as ReactElement).props.id === target.id);
-
-        const nextIndex = childIndex + movementDirection;
-        if (nextIndex >= totalNumberOfChildren) {
-            focusChild(0);
-        } else if (nextIndex < 0) {
-            focusChild(totalNumberOfChildren - 1);
-        } else {
-            focusChild(nextIndex);
-        }
-    };
-
-    const focusChild = (index: number): void => {
-        // @ts-ignore
-        React.Children.toArray(children)[index].ref.current.focus();
-    };
-
     const leaveFocusListener = useCallback((e: {target: EventTarget | null; key?: string}) => {
+        const setFocusOnExpectedElementWhenUsingUpOrDownKey = (e: { target: EventTarget | null; key?: string }): void => {
+            if (isArrowKeyFunctionalitySetup()) {
+                const movementDirection = getMovementDirection(e.key);
+                setFocusState(e, movementDirection);
+            }
+        };
+
+        const isArrowKeyFunctionalitySetup = (): boolean => {
+            const childrenWithUndefinedId = React.Children.toArray(children).filter(child => (child as ReactElement).props.id === undefined);
+            // @ts-ignore
+            const childrenWithNullRef = React.Children.toArray(children).filter(child => (child as ReactElement).ref === null);
+
+            return childrenWithUndefinedId.length === 0 && childrenWithNullRef.length === 0;
+        };
+
+        const getMovementDirection = (key?: string ): number => {
+            if (key === 'ArrowDown') {
+                return 1;
+            } else {
+                return -1;
+            }
+        };
+
+        const setFocusState = (e: { target: EventTarget | null; key?: string }, movementDirection: number): void => {
+            let target = e.target as HTMLElement;
+
+            const totalNumberOfChildren = React.Children.count(children);
+            const childIndex = React.Children.toArray(children).findIndex(child => (child as ReactElement).props.id === target.id);
+
+            const nextIndex = childIndex + movementDirection;
+            if (nextIndex >= totalNumberOfChildren) {
+                focusChild(0);
+            } else if (nextIndex < 0) {
+                focusChild(totalNumberOfChildren - 1);
+            } else {
+                focusChild(nextIndex);
+            }
+        };
+
+        const focusChild = (index: number): void => {
+            // @ts-ignore
+            React.Children.toArray(children)[index].ref.current.focus();
+        };
+
         if (!dropdownContainer.current?.contains(e.target as HTMLElement) && !dontCloseForTheseIds?.includes((e.target as HTMLElement).id)) {
             handleClose();
         }
@@ -94,7 +94,6 @@ export default function AccessibleDropdownContainer({handleClose, ariaLabelledBy
         }
     }, [dropdownContainer,
         handleClose,
-        setFocusOnExpectedElementWhenUsingUpOrDownKey,
         dontCloseForTheseIds]);
 
 
