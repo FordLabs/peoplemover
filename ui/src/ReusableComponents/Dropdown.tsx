00107ba279d34b897455e9b2a14c4091b54c9f86
jwcherney <72267250+jwcherney@users.noreply.github.com>
2021-04-08 16:06:25 -0400

    148 show number of filter results (#664)
    Co-authored-by: Vianney Willot <vianney.willot@gmail.com>
    Co-authored-by: John Cherney <jwcherney@hotmail.com>

--- ui/src/ReusableComponents/Dropdown.tsx
+++ ui/src/ReusableComponents/Dropdown.tsx
@@ -1,3 +1,20 @@
+/*!
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import AccessibleDropdownContainer from './AccessibleDropdownContainer';
 import React, {ReactNode, useState} from 'react';
 
@@ -9,9 +26,10 @@ interface DropdownProps {
     dropdownTestId?: string;
     buttonTestId?: string;
     closeOnSelect?: boolean;
+    clearFilterButton?: ReactNode;
 }
 
-export default function Dropdown({buttonId, dropdownButtonContent, dropdownContent, dropdownOptionIds, dropdownTestId, buttonTestId, closeOnSelect = false}: DropdownProps): JSX.Element {
+export default function Dropdown({buttonId, dropdownButtonContent, dropdownContent, dropdownOptionIds, dropdownTestId, buttonTestId, closeOnSelect = false, clearFilterButton}: DropdownProps): JSX.Element {
 
     const [dropdownToggle, setDropdownToggle] = useState<boolean>(false);
 
@@ -50,6 +68,7 @@ export default function Dropdown({buttonId, dropdownButtonContent, dropdownConte
 
                     </AccessibleDropdownContainer>
                 </div>}
+            {clearFilterButton}
         </div>
     );
 }
