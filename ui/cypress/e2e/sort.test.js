c3183dcd6ca0e132e39e394a29e8872493c30ed4
John Cherney <jwcherney@hotmail.com>
2021-04-20 14:55:10 -0400

    Update cypress tests
    Co-authored-by: John Cherney <jwcherney@hotmail.com>
    Co-authored-by: Stephen Graham <sgraha75@ford.com>

--- ui/cypress/e2e/sort.test.js
+++ ui/cypress/e2e/sort.test.js
@@ -17,80 +17,9 @@
 
 /// <reference types="Cypress" />
 
-describe('Sort for old ui', () => {
-    beforeEach(() => {
-        cy.visitSpace();
-        cy.server();
-    });
-
-    it('Old sort products alphabetically', () => {
-        cy.get('[data-testid=productCardContainer__my_product]');
-        cy.get('[data-testid=productCardContainer__baguette_bakery]');
-
-        cy.selectOptionFromReactSelect('[data-testid=sortBy]', 'Alphabetical')
-            .then(() => {
-                cy.get('[data-testid=productListSortedContainer]')
-                    .find('[data-testid*=productCardContainer__]')
-                    .should('have.length', 2)
-                    .eq(0)
-                    .should('contain', 'Baguette Bakery');
-
-                cy.get('[data-testid=productListSortedContainer]')
-                    .find('[data-testid*=productCardContainer__]')
-                    .eq(1)
-                    .should('contain', 'My Product');
-
-            });
-    });
-
-    it('Old sort products by location', () => {
-        cy.get('[data-testid=productCardContainer__my_product]');
-        cy.get('[data-testid=productCardContainer__baguette_bakery]');
-
-        cy.selectOptionFromReactSelect('[data-testid=sortBy]', 'Location')
-            .then(() => {
-                cy.get('[data-testid=productListGroupedContainer]')
-                    .find('[data-testid=productGroup]')
-                    .should('have.length', 2)
-                    .eq(0)
-                    .should('contain', 'Baguette Bakery')
-                    .should('contain', 'location1');
-
-                cy.get('[data-testid=productListGroupedContainer]')
-                    .find('[data-testid=productGroup]')
-                    .eq(1)
-                    .should('contain', 'My Product')
-                    .should('contain', 'No Location');
-
-            });
-    });
-
-    it('Old sort products by product tag', () => {
-        cy.get('[data-testid=productCardContainer__my_product]');
-        cy.get('[data-testid=productCardContainer__baguette_bakery]');
-
-        cy.selectOptionFromReactSelect('[data-testid=sortBy]', 'Product Tag')
-            .then(() => {
-                cy.get('[data-testid=productListGroupedContainer]')
-                    .find('[data-testid=productGroup]')
-                    .should('have.length', 2)
-                    .eq(0)
-                    .should('contain', 'My Product')
-                    .should('contain', 'productTag1');
-
-                cy.get('[data-testid=productListGroupedContainer]')
-                    .find('[data-testid=productGroup]')
-                    .eq(1)
-                    .should('contain', 'Baguette Bakery')
-                    .should('contain', 'No Product Tag');
-
-            });
-    });
-});
-
 describe('Sort', () => {
     beforeEach(() => {
-        cy.visitSpace({}, '#newui');
+        cy.visitSpace();
         cy.server();
     });
 
