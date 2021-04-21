a6dfe40a02701525cb686f17c5df2824e5d704c1
John Cherney <jwcherney@hotmail.com>
2021-04-21 14:41:27 -0400

    Add 'Contact Us' and remove underline from 'a' tags
    Co-authored-by: John Cherney <jwcherney@hotmail.com>
    Co-authored-by: Stephen Graham <sgraha75@ford.com>

--- ui/src/ReusableComponents/Branding.tsx
+++ ui/src/ReusableComponents/Branding.tsx
@@ -21,6 +21,13 @@ import FordLabsLogo from '../Application/Assets/fordlabs_logo.svg';
 
 function Branding(): JSX.Element {
     const fordLabsUrl = window?.runConfig?.ford_labs_url || '';
+    const padLeftAndRemoveUnderline = {
+        textDecoration: 'none',
+        paddingLeft: '6px',
+    };
+    const removeUnderline = {
+        textDecoration: 'none',
+    };
     return (
         <div className="brandingContainer"
             aria-label="Powered by Ford Labs">
@@ -34,9 +41,13 @@ function Branding(): JSX.Element {
             <a target="_blank"
                 rel="noopener noreferrer"
                 href={fordLabsUrl}
+                style={removeUnderline}
                 className="brandingMessage">
                 FordLabs
             </a>
+            <span className="brandingMessage" style={padLeftAndRemoveUnderline}>|
+                <a href="mailto:matieh@ford.com" className="brandingMessage" style={padLeftAndRemoveUnderline}>Contact Us</a>
+            </span>
         </div>
     );
 }
