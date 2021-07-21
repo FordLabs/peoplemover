712066ec107889363a9862bd80b9cd3856fe7ef3
John Cherney <jwcherney@hotmail.com>
2021-07-21 13:57:16 -0400

    Add url field to add/edit product form

--- ui/src/Products/ProductForm.tsx
+++ ui/src/Products/ProductForm.tsx
@@ -190,10 +190,21 @@ function ProductForm({
                         onChange={(e: ChangeEvent<HTMLInputElement>): void => updateProductField('name', e.target.value)}
                         placeholder="e.g. Product 1"/>
                     {nameWarningMessage &&
-                        <span data-testid="productNameWarningMessage" className="productNameWarning">
-                            {nameWarningMessage}
-                        </span>
+                    <span data-testid="productNameWarningMessage" className="productNameWarning">
+                        {nameWarningMessage}
+                    </span>
                     }
+                </div>
+                <div className="formItem">
+                    <label className="formItemLabel" htmlFor="url">URL</label>
+                    <input className="formInput formTextInput"
+                        data-testid="productFormUrlField"
+                        type="text"
+                        name="url"
+                        id="url"
+                        value={currentProduct.url}
+                        onChange={(e: ChangeEvent<HTMLInputElement>): void => updateProductField('url', e.target.value)}
+                        placeholder="e.g. https://mach-e.ford.com/"/>
                 </div>
                 <ProductFormLocationField
                     currentProductState={{currentProduct, setCurrentProduct}}
