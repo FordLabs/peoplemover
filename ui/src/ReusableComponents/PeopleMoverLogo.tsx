9a70d11c800669be3793445ca42977a9eefcbe5a
Naseem Bokhari <nb00000@mailfence.com>
2021-07-27 14:10:02 -0400

    changed people mover logo to smaller file size

--- ui/src/ReusableComponents/PeopleMoverLogo.tsx
+++ ui/src/ReusableComponents/PeopleMoverLogo.tsx
@@ -16,7 +16,7 @@
  */
 
 import React from 'react';
-import PersonIcon from '../Application/Assets/logo.svg';
+import PersonIcon from '../Application/Assets/small-logo.png';
 
 import './PeopleMoverLogo.scss';
 
