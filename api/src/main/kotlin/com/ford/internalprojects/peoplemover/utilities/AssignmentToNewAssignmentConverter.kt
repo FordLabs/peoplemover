9268ab8c4cbed8e388e06248bb3b60f792c9e4c8
John Cherney <jwcherney@hotmail.com>
2021-08-16 11:34:51 -0400

    Remove grouping assignments by space. Not needed. Other minor optimizations

--- api/src/main/kotlin/com/ford/internalprojects/peoplemover/utilities/AssignmentToNewAssignmentConverter.kt
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/utilities/AssignmentToNewAssignmentConverter.kt
@@ -10,35 +10,16 @@ class AssignmentToNewAssignmentConverter {
     fun convert(oldAssignments: List<Assignment>): List<NewAssignment> {
         val listOfNewAssignments = mutableListOf<NewAssignment>()
 
-        val mapOfSpace = mapSpacesToAssignments(oldAssignments)
-        for (assignmentInSpace in mapOfSpace.values) {
+        val mapOfPerson = mapPersonToAssignments(oldAssignments)
 
-            val mapOfPerson = mapPersonToAssignments(assignmentInSpace)
-
-            for (list in mapOfPerson.values) {
-                listOfNewAssignments.addAll(createListOfNewAssignmentForAPerson(list))
-            }
+        for (list in mapOfPerson.values) {
+            listOfNewAssignments.addAll(createListOfNewAssignmentForAPerson(list))
         }
-        return listOfNewAssignments
+        return listOfNewAssignments.toList()
     }
 
-    private fun mapSpacesToAssignments(oldAssignments: List<Assignment>): MutableMap<String, MutableList<Assignment>> {
-            val mapOfSpace = mutableMapOf<String,MutableList<Assignment>>();
-
-            for (assignment in oldAssignments) {
-                if(!mapOfSpace.containsKey(assignment.spaceUuid)) {
-                    mapOfSpace[assignment.spaceUuid] = mutableListOf(assignment)
-                } else {
-                    val oldList = mapOfSpace[assignment.spaceUuid]
-                    oldList!!.add(assignment)
-                    mapOfSpace[assignment.spaceUuid] = oldList
-                }
-            }
-            return  mapOfSpace
-    }
-
-    private fun createListOfNewAssignmentForAPerson(oldAssignments: List<Assignment>): MutableList<NewAssignment> {
-        var returnList = mutableListOf<NewAssignment>()
+    private fun createListOfNewAssignmentForAPerson(oldAssignments: List<Assignment>): List<NewAssignment> {
+        val returnList = mutableListOf<NewAssignment>()
         val sortedAssignments = oldAssignments.sortedBy { it.effectiveDate }.toMutableList()
         val listOfDateSorted = sortedAssignments.stream().map { assignment -> assignment.effectiveDate }.distinct().toList()
 
@@ -46,7 +27,7 @@ class AssignmentToNewAssignmentConverter {
         for (currentDate in listOfDateSorted) {
             val assignmentsForDate = currentDate?.let { findAssignmentsEffectiveOnDate(it, sortedAssignments) };
             if (assignmentsForDate != null) {
-                endAssignments(identifyAssignmentsEnding(returnList, assignmentsForDate, currentDate), currentDate)
+                endAssignments(identifyAssignmentsEnding(returnList, assignmentsForDate), currentDate)
                 for (a in assignmentsForDate) {
                     if (!isAssignedToProductOnDate(a.productId, currentDate, returnList)) {
                         returnList.add(NewAssignment(person = a.person, placeholder = a.placeholder, productId = a.productId, startDate = currentDate, endDate = null, spaceUuid = a.spaceUuid));
@@ -54,7 +35,7 @@ class AssignmentToNewAssignmentConverter {
                 }
             }
         }
-        return returnList;
+        return returnList.toList();
     }
 
     private fun mapPersonToAssignments(oldAssignments: List<Assignment>): Map<Person, List<Assignment>> {
@@ -69,27 +50,27 @@ class AssignmentToNewAssignmentConverter {
                 mapOfPerson[assignment.person] = oldList
             }
         }
-        return  mapOfPerson
+        return  mapOfPerson.toMap()
     }
 
-    private fun endAssignments(assns: MutableList<NewAssignment>, date: LocalDate){
+    private fun endAssignments(assns: List<NewAssignment>, date: LocalDate){
         for(assn in assns){
             assn.endDate = date;
         }
     }
 
-    private fun identifyAssignmentsEnding(newAssignments: MutableList<NewAssignment>, assignmentsForDate: List<Assignment>, date: LocalDate) : MutableList<NewAssignment> {
-        val toReturn : MutableList<NewAssignment> = mutableListOf<NewAssignment>();
+    private fun identifyAssignmentsEnding(newAssignments: List<NewAssignment>, assignmentsForDate: List<Assignment>) : List<NewAssignment> {
+        val toReturn = mutableListOf<NewAssignment>();
         val outstandingNewAssignments: List<NewAssignment> = findOutstandingAssignments(newAssignments);
         for (newAssignment in outstandingNewAssignments){
             if(assignmentsForDate.count { a -> a.productId == newAssignment.productId} <= 0){
                 toReturn.add(newAssignment);
             }
         }
-        return toReturn;
+        return toReturn.toList()
     }
 
-    fun isAssignedToProductOnDate(productId: Int, date: LocalDate, assignments: List<NewAssignment> ): Boolean{
+    private fun isAssignedToProductOnDate(productId: Int, date: LocalDate, assignments: List<NewAssignment> ): Boolean{
         for(assignment in assignments.filter{assn -> assn.productId == productId})
         {
             if(assignment.startDate.isBefore(date) && (assignment.endDate == null || assignment.endDate!!.isAfter(date))){
@@ -99,11 +80,11 @@ class AssignmentToNewAssignmentConverter {
         return false;
     }
 
-    fun findOutstandingAssignments(assignments: List<NewAssignment>) : List<NewAssignment>{
+    private fun findOutstandingAssignments(assignments: List<NewAssignment>) : List<NewAssignment>{
         return assignments.filter { assignment -> assignment.endDate == null }
     }
 
-    fun findAssignmentsEffectiveOnDate(date: LocalDate, assignments: List<Assignment>) : List<Assignment> {
+    private fun findAssignmentsEffectiveOnDate(date: LocalDate, assignments: List<Assignment>) : List<Assignment> {
         return assignments.filter { assignment -> assignment.effectiveDate == date }
     }
 }
--- api/src/test/kotlin/com/ford/internalprojects/peoplemover/utilities/AssignmentToNewAssignmentConverterTest.kt
+++ api/src/test/kotlin/com/ford/internalprojects/peoplemover/utilities/AssignmentToNewAssignmentConverterTest.kt
@@ -12,7 +12,7 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `can convert one old assignment to new one with no end date`() {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var testPerson = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
         val juneAssignment = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
 
         var expectedNewAssignment = NewAssignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-06-06"), endDate=null)
@@ -23,7 +23,7 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `can merge two old assignments to the same product that are contiguous`() {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var testPerson = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
         val juneAssignment = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
         val julyAssignment = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-07-06"))
 
@@ -37,7 +37,7 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `can merge two old assignments to the same product that are contiguous when there's one that isn't the same product`() {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var testPerson = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
         val juneAssignment = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
         val assignment4 = Assignment(person = testPerson, productId = 2, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-12"))
         val julyAssignment = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-07-06"))
@@ -53,7 +53,7 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `can merge  with multiple concomitant assignments` () {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var testPerson = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
         val prod1assignment1 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
         val prod1assignment2 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-16"))
         val prod2assignment1 = Assignment(person = testPerson, productId = 2, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-16"))
@@ -78,7 +78,7 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `non-contiguous assignments to one project` () {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var testPerson = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
         val prod1assignment1 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
         val prod1assignment2 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-16"))
         val prod1assignment3 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-26"))
@@ -97,8 +97,8 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `can distinguish between assignments of two different people` () {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
-        var notTheTestPerson = Person(name = "Roger Rabbit", spaceUuid = "Outer Space")
+        var testPerson = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var notTheTestPerson = Person(id = 2, name = "Roger Rabbit", spaceUuid = "Outer Space")
         val prod1assignment1 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
         val prod1assignment2 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-16"))
         val prod1assignment3 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-26"))
@@ -117,18 +117,19 @@ internal class AssignmentToNewAssignmentConverterTest {
 
     @Test
     fun `can distinguish between assignments in two different spaces` () {
-        var testPerson = Person(name = "Bugs Bunny", spaceUuid = "Outer Space")
-        val prod1assignment1 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
-        val prod1assignment2 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-16"))
-        val prod1assignment3 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-26"))
-        val prod3assignment1 = Assignment(person = testPerson, productId = 3, spaceUuid = "Inner Space", effectiveDate = LocalDate.parse("2021-07-06"))
-        val prod4assignment1 = Assignment(person = testPerson, productId = 4, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-07-28"))
-        val prod1assignment4 = Assignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-07-30"))
+        var testPerson1 = Person(id = 1, name = "Bugs Bunny", spaceUuid = "Outer Space")
+        var testPerson2 = Person(id = 2, name = "Bugs Bunny", spaceUuid = "Inner Space")
+        val prod1assignment1 = Assignment(person = testPerson1, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-06"))
+        val prod1assignment2 = Assignment(person = testPerson1, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-16"))
+        val prod1assignment3 = Assignment(person = testPerson1, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-06-26"))
+        val prod3assignment1 = Assignment(person = testPerson2, productId = 3, spaceUuid = "Inner Space", effectiveDate = LocalDate.parse("2021-07-06"))
+        val prod4assignment1 = Assignment(person = testPerson1, productId = 4, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-07-28"))
+        val prod1assignment4 = Assignment(person = testPerson1, productId = 1, spaceUuid = "Outer Space", effectiveDate = LocalDate.parse("2021-07-30"))
 
-        val expectedProd1 = NewAssignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-06-06"), endDate= LocalDate.parse("2021-07-28"))
-        val expectedProd3 = NewAssignment(person = testPerson, productId = 3, spaceUuid = "Inner Space", startDate = LocalDate.parse("2021-07-06"), endDate= null)
-        val expectedProd4 = NewAssignment(person = testPerson, productId = 4, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-07-28"), endDate= LocalDate.parse("2021-07-30"))
-        val expectedProd1Again = NewAssignment(person = testPerson, productId = 1, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-07-30"), endDate= null)
+        val expectedProd1 = NewAssignment(person = testPerson1, productId = 1, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-06-06"), endDate= LocalDate.parse("2021-07-28"))
+        val expectedProd3 = NewAssignment(person = testPerson2, productId = 3, spaceUuid = "Inner Space", startDate = LocalDate.parse("2021-07-06"), endDate= null)
+        val expectedProd4 = NewAssignment(person = testPerson1, productId = 4, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-07-28"), endDate= LocalDate.parse("2021-07-30"))
+        val expectedProd1Again = NewAssignment(person = testPerson1, productId = 1, spaceUuid = "Outer Space", startDate = LocalDate.parse("2021-07-30"), endDate= null)
 
         val conversionResult : List<NewAssignment> = AssignmentToNewAssignmentConverter().convert(listOf(prod1assignment1, prod1assignment2,prod1assignment3,prod3assignment1,prod4assignment1,prod1assignment4));
         assertThat(conversionResult).containsExactlyInAnyOrderElementsOf(listOf(expectedProd1,expectedProd3,expectedProd4,expectedProd1Again));
