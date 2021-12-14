8eb83dc73c5184fd06a7fa0476023444182d4f5b
Connor Shaughnessy <sapphon@gmail.com>
2021-12-14 17:46:10 -0500

    [#282] PersonImportController exists and provides template
    Co-authored-by: Connor Shaughnessy <sapphon@gmail.com>
    Co-authored-by: Alexandria Kudlinski <amkudlin@umich.edu>

---
+++ api/src/main/kotlin/com/ford/internalprojects/peoplemover/person/PersonImportController.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ford.internalprojects.peoplemover.person
+
+import com.ford.internalprojects.peoplemover.utilities.BasicLogger
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RequestMapping("/api/spaces/{spaceUuid}/people/import")
+@RestController
+class PersonImportController(
+        private val logger: BasicLogger,
+        private val personService: PersonService
+) {
+
+    @PreAuthorize("hasPermission(#spaceUuid, 'read')")
+    @GetMapping
+    fun getImportTemplate(@PathVariable spaceUuid: String): String {
+        return "Person Name\tCDSID\tPerson Role\tPerson Note\tPerson Tags\r\nBruce Wayne\timbatman\tSuperhero\tLikes champagne\tNight Shift";
+    }
+
+    @PreAuthorize("hasPermission(#spaceUuid, 'write')")
+    @PostMapping
+    fun importPeople(
+            @PathVariable spaceUuid: String,
+            @Valid @RequestBody personIncoming: PersonImportRequest
+    ): Boolean {
+        return false;
+    }
+class PersonImportRequest{
+
+}
+
+}
---
+++ api/src/test/kotlin/com/ford/internalprojects/peoplemover/person/PersonImportControllerTest.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2021 Ford Motor Company
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ford.internalprojects.peoplemover.person
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ford.internalprojects.peoplemover.assignment.*
+import com.ford.internalprojects.peoplemover.auth.PERMISSION_OWNER
+import com.ford.internalprojects.peoplemover.auth.UserSpaceMapping
+import com.ford.internalprojects.peoplemover.auth.UserSpaceMappingRepository
+import com.ford.internalprojects.peoplemover.product.Product
+import com.ford.internalprojects.peoplemover.product.ProductRepository
+import com.ford.internalprojects.peoplemover.space.Space
+import com.ford.internalprojects.peoplemover.space.SpaceRepository
+import com.ford.internalprojects.peoplemover.space.SpaceService
+import com.ford.internalprojects.peoplemover.tag.location.SpaceLocationRepository
+import com.ford.internalprojects.peoplemover.tag.person.PersonTag
+import com.ford.internalprojects.peoplemover.tag.person.PersonTagRepository
+import com.ford.internalprojects.peoplemover.tag.role.SpaceRole
+import com.ford.internalprojects.peoplemover.tag.role.SpaceRolesRepository
+import com.ford.internalprojects.peoplemover.utilities.EMPTY_NAME
+import com.ford.internalprojects.peoplemover.utilities.CHAR_260
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.LocalDate
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner::class)
+class PersonImportControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var spaceService: SpaceService
+
+    @Autowired
+    private lateinit var spaceRepository: SpaceRepository
+
+    @Autowired
+    private lateinit var spaceRolesRepository: SpaceRolesRepository
+
+    @Autowired
+    private lateinit var personRepository: PersonRepository
+
+    @Autowired
+    private lateinit var assignmentService: AssignmentService
+
+    @Autowired
+    private lateinit var assignmentRepository: AssignmentRepository
+
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    @Autowired
+    private lateinit var spaceLocationRepository: SpaceLocationRepository
+
+    @Autowired
+    private lateinit var personTagRepository: PersonTagRepository
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var userSpaceMappingRepository: UserSpaceMappingRepository
+
+    private lateinit var space: Space
+    private lateinit var spaceTwo: Space
+
+    private lateinit var tag: PersonTag
+
+    var basePeopleUrl: String = ""
+
+    private fun getBaseImportUrl(spaceUuid: String) = "/api/spaces/$spaceUuid/people/import"
+
+    @Before
+    fun setUp() {
+        space = spaceService.createSpaceWithName("spaceWithThisName", "Nobody")
+        spaceTwo = spaceService.createSpaceWithName("spaceThatUserDoesNotHaveAccessTo", "Nobody")
+
+        tag = personTagRepository.save(PersonTag(spaceUuid = space.uuid, name = "Agency Employee" ))
+
+        basePeopleUrl = getBaseImportUrl(space.uuid)
+
+        userSpaceMappingRepository.save(UserSpaceMapping(userId = "USER_ID", spaceUuid = space.uuid, permission = PERMISSION_OWNER))
+    }
+
+    @After
+    fun tearDown() {
+        assignmentRepository.deleteAll()
+        spaceRolesRepository.deleteAll()
+        personRepository.deleteAll()
+        productRepository.deleteAll()
+        spaceLocationRepository.deleteAll()
+        spaceRepository.deleteAll()
+        personTagRepository.deleteAll()
+    }
+
+    @Test
+    fun `requesting a download template always gets you the same (correct) file every time`(){
+    var expected = "Person Name\tCDSID\tPerson Role\tPerson Note\tPerson Tags\r\nBruce Wayne\timbatman\tSuperhero\tLikes champagne\tNight Shift"
+        val mvcResult = mockMvc.perform(get(getBaseImportUrl(space.uuid))
+                .header("Authorization", "Bearer GOOD_TOKEN"))
+                .andExpect(status().isOk)
+                .andReturn();
+        assertThat(mvcResult.response.contentAsString).isEqualTo(expected)
+    }
+}
