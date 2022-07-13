1707a102474258ddecbb350bcf88fa9582093543
Ashley Clifton <randomashes@gmail.com>
2022-07-13 14:58:43 -0400

    Write tests for PersonTagsFilter.tsx

--- ui/src/SortingAndFiltering/PersonTagsFilter/PersonTagsFilter.test.tsx
+++ ui/src/SortingAndFiltering/PersonTagsFilter/PersonTagsFilter.test.tsx
@@ -17,23 +17,23 @@
 
 import React from 'react';
 import {renderWithRecoil} from '../../Utils/TestUtils';
-import RolesFilter from './RolesFilter';
 import {screen, waitFor} from '@testing-library/react';
-import {RolesState} from '../../State/RolesState';
 import TestData from '../../Utils/TestData';
 import {LocalStorageFilters} from '../FilterLibraries';
 import {ModalContents, ModalContentsState} from '../../State/ModalContentsState';
 import {RecoilObserver} from '../../Utils/RecoilObserver';
-import MyRolesForm from '../../Roles/MyRolesForm';
 import {localStorageEventListenerKey} from '../../Hooks/useOnStorageChange/useOnStorageChange';
+import MyTagsForm from '../../Tags/MyTagsForm';
+import PersonTagsFilter from './PersonTagsFilter';
+import {PersonTagsState} from '../../State/PersonTagsState';
 
-describe('Role Filter', () => {
+describe('Person Tags Filter', () => {
     let modalContent: ModalContents | null;
     const initialFilters: LocalStorageFilters = {
         locationTagFilters: [],
         productTagFilters: [],
-        roleTagFilters: [TestData.softwareEngineer.name],
-        personTagFilters: [],
+        roleTagFilters: [],
+        personTagFilters: [TestData.personTag1.name],
     };
 
     beforeEach(() => {
@@ -48,61 +48,59 @@ describe('Role Filter', () => {
                         modalContent = value;
                     }}
                 />
-                <RolesFilter />
+                <PersonTagsFilter />
             </>,
             ({set}) => {
-                set(RolesState, TestData.roles)
+                set(PersonTagsState, TestData.personTags)
             })
     })
 
-    it('should show "Role:" label', () => {
-        expect(getRoleDropdownButton()).toHaveTextContent('Role:');
+    it('should show "Person Tags:" label', () => {
+        expect(getPersonTagsDropdownButton()).toHaveTextContent('Person Tags:');
     });
 
-    it('should show only roles saved in local storage as selected', () => {
-        getRoleDropdownButton().click();
-        const SECheckbox = screen.getByLabelText(TestData.softwareEngineer.name);
-        expect(SECheckbox).toBeChecked();
-        const PMCheckbox = screen.getByLabelText(TestData.productManager.name);
-        expect(PMCheckbox).not.toBeChecked();
-        const PDCheckbox = screen.getByLabelText(TestData.productDesigner.name);
-        expect(PDCheckbox).not.toBeChecked();
+    it('should show only person tags saved in local storage as selected', () => {
+        getPersonTagsDropdownButton().click();
+        const personTag1Checkbox = screen.getByLabelText(TestData.personTag1.name);
+        expect(personTag1Checkbox).toBeChecked();
+        const personTag2Checkbox = screen.getByLabelText(TestData.personTag2.name);
+        expect(personTag2Checkbox).not.toBeChecked();
     });
 
-    it('should update local storage and trigger storage event when role option is checked or unchecked', () => {
-        const getPMCheckbox = () => screen.getByLabelText(TestData.productManager.name);
+    it('should update local storage and trigger storage event when product location option is checked or unchecked', () => {
+        const getPersonTag2Checkbox = () => screen.getByLabelText(TestData.personTag2.name);
         const dispatchEvent = jest.spyOn(window, 'dispatchEvent');
 
-        getRoleDropdownButton().click();
-        getPMCheckbox().click();
+        getPersonTagsDropdownButton().click();
+        getPersonTag2Checkbox().click();
 
-        expect(getPMCheckbox()).toBeChecked();
+        expect(getPersonTag2Checkbox()).toBeChecked();
         expect(dispatchEvent).toHaveBeenCalledTimes(1);
         expect(dispatchEvent).toHaveBeenCalledWith(new Event(localStorageEventListenerKey))
         expect(localStorage.getItem('filters')).toEqual(JSON.stringify({
             locationTagFilters: [],
             productTagFilters: [],
-            roleTagFilters: [TestData.productManager.name, TestData.softwareEngineer.name],
-            personTagFilters: [],
+            roleTagFilters: [],
+            personTagFilters: [TestData.personTag1.name, TestData.personTag2.name],
         }))
 
-        getPMCheckbox().click();
+        getPersonTag2Checkbox().click();
 
         expect(dispatchEvent).toHaveBeenCalledTimes(2);
         expect(dispatchEvent).toHaveBeenCalledWith(new Event(localStorageEventListenerKey))
-        expect(getPMCheckbox()).not.toBeChecked();
+        expect(getPersonTag2Checkbox()).not.toBeChecked();
         expect(localStorage.getItem('filters')).toEqual(JSON.stringify(initialFilters))
     });
 
-    it('should open roles modal when user selects "Add/Edit your Role" from the dropdown', async () => {
-        getRoleDropdownButton().click();
-        const openModalButton = screen.getByText('Add/Edit your Role');
+    it('should open person tag modal when user selects "Add/Edit your Person Tags" from the dropdown', async () => {
+        getPersonTagsDropdownButton().click();
+        const openModalButton = screen.getByText('Add/Edit your Person Tags');
         openModalButton.click();
-        await waitFor(() => expect(modalContent?.title).toBe('My Roles'));
-        await waitFor(() => expect(modalContent?.component.type).toBe(MyRolesForm));
+        await waitFor(() => expect(modalContent?.title).toBe('Person Tags'));
+        await waitFor(() => expect(modalContent?.component.type).toBe(MyTagsForm));
     });
 });
 
-function getRoleDropdownButton() {
-    return screen.getByTestId('dropdownButton__role')
+function getPersonTagsDropdownButton() {
+    return screen.getByTestId('dropdownButton__person_tags')
 }
